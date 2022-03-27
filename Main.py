b32e1dcde070f76898adc1ca080f55b0c3752de6
AsafDov <Nathendov@gmail.com>
2022-03-27 22:35:43 +0300

    First commit

--- Main.py
+++ Main.py
@@ -19,48 +19,48 @@ from matplotlib import pyplot as plt;
 import time
 
 brain = Perceptron(2);
-points = [] #List of Points()
+points = []  # List of Points()
 
-fig = plt.figure(); # The plotting figure
+fig = plt.figure()  # The plotting figure
 
-#Create training data and plot them. Range is number of points for training
+# Create training data and plot them. Range is number of points for training
 for i in range(10):
-    points.append(Point());
-   # print(point[i].pt,point[i].label)
-    if(points[i].label == 1):
-        plt.scatter(points[i].pt[0], points[i].pt[1], marker="o" ,edgecolors="green",color='green') # x>y
+    points.append(Point())
+    # print(point[i].pt,point[i].label)
+
+    if points[i].label == 1:
+        plt.scatter(points[i].pt[0], points[i].pt[1], marker="o", edgecolors="green", color='green')  # x>y
     else:
-        plt.scatter(points[i].pt[0], points[i].pt[1], marker="o",edgecolor="black",color = 'black') # x<y
+        plt.scatter(points[i].pt[0], points[i].pt[1], marker="o", edgecolor="black", color='black')  # x<y
 
-#LINE of X=Y
-plt.plot(np.array([-1, 1]),np.array([-1,1]))
+# Plot line of X=Y
+plt.plot(np.array([-1, 1]), np.array([-1, 1]))
 
+# GUESS: You can set a point and let the untrained machine guess. black means wrong, green means right.
+# for pnt in points:
+#     x = pnt.pt[0]
+#     y = pnt.pt[1]
+#     prediction = brain.predict(pnt.pt)
+#     if prediction == pnt.label:
+#         plt.scatter(x, y, marker="o", color="green")
+#     else:
+#         plt.scatter(x, y, marker="o", color="black")
+#     print(brain.predict(pnt.pt) == pnt.label)
 
-# GUESS:
-for pnt in points:
-    if (brain.predict(pnt.pt) == 1):
-        plt.scatter(pnt.pt[0], pnt.pt[1], marker = "o", color="green")
-    else:
-        plt.scatter(pnt.pt[0], pnt.pt[1], marker = "o", color="black")
-    print(brain.predict(pnt.pt) == pnt.label)
-        
-
-
-# train
+# Training the perceptron
 numberOFItrations = 10
 for i in range(numberOFItrations):
-    numOfErrors = 0;
-    for pnt in points:    
+    numOfErrors = 0
+    for pnt in points:
         error = brain.train(pnt.pt, pnt.label);
-        if error==0:
-            plt.scatter(pnt.pt[0], pnt.pt[1],marker = "o",color='green')
-        else: 
-            numOfErrors +=1
-            plt.scatter(pnt.pt[0], pnt.pt[1],marker = "o", color='black')
-    print(numOfErrors)
+        if error == 0:
+            plt.scatter(pnt.pt[0], pnt.pt[1], marker="o", color='green')
+        else:
+            numOfErrors += 1
+            plt.scatter(pnt.pt[0], pnt.pt[1], marker="o", color='black')
+    print("Iteration #" + str(i) + "\n" + "Error Points:" + str(numOfErrors))
 
     # SHOW plot
-    plt.plot(np.array([-1, 1]),np.array([-1,1]))
-    plt.show()      
+    plt.plot(np.array([-1, 1]), np.array([-1, 1]))
+    plt.show()
     time.sleep(1)
-        
--- TrainingPoints.py
+++ TrainingPoints.py
@@ -4,14 +4,15 @@ Created on Sat Apr 24 18:47:14 2021
 
 @author: asafd
 """
-import numpy as np;
+import numpy as np
+
+
 class Point:
-    
+
     def __init__(self):
-       self.pt = np.random.rand(2,1) *2 -1;
-       
-       if(self.pt[0]>=self.pt[1]):
-           self.label = 1;
-       else:
-           self.label = -1;
-           
+        self.pt = np.random.rand(2, 1) * 2 - 1
+
+        if self.pt[0] >= self.pt[1]:
+            self.label = 1
+        else:
+            self.label = -1
